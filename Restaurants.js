0b16aa0768085f3346b426dc2241e7b1eae8583e
s2004607 <r.r.govers@student.utwente.nl>
2020-09-15 17:11:39 +0200

    Very basic restaurant API

---
+++ Restaurants.js
@@ -0,0 +1,19 @@
+const restaurants = [
+  {
+    id: 1,
+    name: 'McDonalds',
+    location: 'Enschede Centrum'
+  },
+  {
+    id: 2,
+    name: 'Burgerking',
+    location: 'Enschede Stationsplein'
+  },
+  {
+    id: 3,
+    name: 'McDonals',
+    location: 'Enschede Woonplein'
+  }
+];
+
+module.exports = restaurants;
