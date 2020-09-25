8494a5532117c448a3f2f0b579342d72d66fa00e
Konstantin <konstantinas.averkin@gmail.com>
2020-09-25 18:38:31 +0200

    updated db and sql.js

--- appdb.sql
+++ appdb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 25, 2020 at 04:08 PM
+-- Generation Time: Sep 25, 2020 at 06:36 PM
 -- Server version: 10.1.32-MariaDB
 -- PHP Version: 7.2.5
 
@@ -20,6 +20,20 @@ SET time_zone = "+00:00";
 --
 -- Database: `appdb`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `checkin`
+--
+
+CREATE TABLE `checkin` (
+  `id` int(11) NOT NULL,
+  `userid` int(11) NOT NULL,
+  `restid` int(11) NOT NULL,
+  `at_risk` tinyint(1) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -41,7 +55,7 @@ CREATE TABLE `restaurants` (
 INSERT INTO `restaurants` (`id`, `name`, `location`, `contact`) VALUES
 (1, 'Restaurant LaRoche', 'Hengelosestraat 200, 7521 AL Enschede', 'restaurantlaroche.nl'),
 (2, 'De Ouwe Compagnie', 'Walstraat 39, 7511 GG Enschede', 'deouwecompagnie.nl'),
-(3, 'Big Belly\'s Tavern', 'Van Lochemstraat 230, 7511 PM Enschede', 'big-bellys.nl'),
+(3, 'Big Belly\s Tavern', 'Van Lochemstraat 230, 7511 PM Enschede', 'big-bellys.nl'),
 (4, 'burgerme Enschede', 'Willem Wilminkplein 27, 7511 PP Enschede', 'burgerme.nl'),
 (5, 'Blue Sakura', 'Oude Markt 21, 7511 GB Enschede', 'bluesakura.nl'),
 (6, 'Asaka', 'Zuiderhagen 53, 7511 GK Enschede', 'asaka.nl'),
@@ -56,7 +70,7 @@ INSERT INTO `restaurants` (`id`, `name`, `location`, `contact`) VALUES
 (15, 'Lunchroom Het Rigtpunt', 'Rigtersbleek-Aalten 4, 7521 RB Enschede', 'lunchroomrigtpunt.nl'),
 (16, 'Heel Bijzonder Enschede', 'De Heurne 47, 7511 GZ Enschede', 'heelbijzonder.com'),
 (17, 'Goodys', 'Marktstraat 2, 7511 GD Enschede', 'goodys.nl'),
-(18, 'Joop\'s Broodjes', 'Deurningerstraat 57, 7514 BD Enschede', 'joopsbroodjes.nl'),
+(18, 'Joop\s Broodjes', 'Deurningerstraat 57, 7514 BD Enschede', 'joopsbroodjes.nl'),
 (19, 'Oh my grill', 'Hengelosestraat 170, 7521 AK Enschede', 'ohmygrill.nl'),
 (20, 'Ana Doner V.O.F.', 'G.J. van Heekstraat 245, 7521 EE Enschede', 'anadoner.ultimatumapp.com'),
 (21, 'Spaans Restaurant Los Sueños \"mas que tapas\" in Enschede', 'Walstraat 41, 7511 GG Enschede', 'lossuenos.nl'),
@@ -67,7 +81,7 @@ INSERT INTO `restaurants` (`id`, `name`, `location`, `contact`) VALUES
 (26, 'Mazza Libanees Restaurant', 'Walstraat 1, 7511 GE Enschede', 'restaurantmazza.nl'),
 (27, 'Restaurant De Tropen', 'Bolwerkstraat 9, 7511 GP Enschede', 'restaurantdetropen.nl'),
 (28, 'Le Mans EET-DRINK & GENIET', 'Brouwerijplein 18, 7523 MB Enschede', 'lemansenschede.nl'),
-(29, 'Verso Cucina Italiana \'d Intenzo', 'Deurningerstraat 11, 7514 BC Enschede', 'restaurantverso.nl'),
+(29, 'Verso Cucina Italiana \d Intenzo', 'Deurningerstraat 11, 7514 BC Enschede', 'restaurantverso.nl'),
 (30, 'Restaurant Turquoise', 'Korte Haaksbergerstraat 3, 7511 JV Enschede', 'turquoise-enschede.nl'),
 (31, 'CopaCabana Steakhouse & Tapasrestaurant', 'Oude Markt 31, 7511 GB Enschede', 'copacabana-cafe.nl'),
 (32, 'Restaurant Het Middelpunt', 'Rembrandtlaan 56, 7545 ZL Enschede', 'middelpunt-enschede.nl'),
@@ -94,19 +108,19 @@ INSERT INTO `restaurants` (`id`, `name`, `location`, `contact`) VALUES
 (53, 'KFC', 'Kalanderstraat 2, 7511 HX Enschede', 'kfc.nl'),
 (54, 'Het Paradijs', 'Nicolaas Beetsstraat 48, 7514 CW Enschede', 'hetparadijs.com'),
 (55, 'Japans Restaurant TAO', 'Deurningerstraat 17, 7514 BC Enschede', 'restauranttao.nl'),
-(56, '\'t Lansink (Hotel - Restaurant)', 'C.T. Storkstraat 18, 7553 AR Hengelo', 'hotellansink.nl'),
-(57, 'Carlina\'s Latin Cuisine', 'Walstraat 69, 7511 GG Enschede', 'carlinas.nl'),
+(56, '\t Lansink (Hotel - Restaurant)', 'C.T. Storkstraat 18, 7553 AR Hengelo', 'hotellansink.nl'),
+(57, 'Carlina\s Latin Cuisine', 'Walstraat 69, 7511 GG Enschede', 'carlinas.nl'),
 (58, 'The Saloon', 'Walstraat 63, 7511 GG Enschede', 'the-saloon.nl'),
 (59, 'Restaurant de Basis', 'Walstraat 15-17, 7511 GE Enschede', 'terugnaardebasis.nu'),
 (60, 'Eethuis Toros', 'Brouwerijplein 28, 7523 MB Enschede', 'eethuistoros.nl'),
 (61, 'De Eetkamer', 'De Bleek 19, 7622 LJ Borne', 'eetkamer-borne.nl'),
 (62, 'Foodbar BLUFF', 'Oude Markt 6, 7511 GA Enschede', 'bluff.nl'),
 (63, 'Thais Restaurant Aroy-D', 'Noorderhagen 20, 7511 EL Enschede', 'aroy-d.nl'),
-(64, 'Hu\'s Garden', 'Oldenzaalsestraat 266, 7523 AG Enschede', 'husgarden.nl'),
+(64, 'Hu\s Garden', 'Oldenzaalsestraat 266, 7523 AG Enschede', 'husgarden.nl'),
 (65, 'Spice King India Enschede', 'Stadsgravenstraat 55, 7511 ER Enschede', 'spicekingindia.com'),
 (66, 'Grieks Restaurant Rhodos', 'Korte Haaksbergerstraat 13, 7511 JV Enschede', 'makro.rest'),
 (67, 'Argentijns restaurant poco mucho', 'Korte Haaksbergerstraat, 7511 JV Enschede', 'pocomucho-enschede.nl'),
-(68, 'Paddy\'s', 'Oude Markt 12, 7511 GA Enschede', 'paddys.nl'),
+(68, 'Paddy\s', 'Oude Markt 12, 7511 GA Enschede', 'paddys.nl'),
 (69, 'Saray Ocakbasi', 'Deurningerstraat 91C, 7514 BE Enschede', ''),
 (70, 'Balkan Restaurant', 'Drienerstraat 29, 7551 HK Hengelo', 'restaurantbalkan.nl'),
 (71, 'Brasserie Kachel', 'Bentrotstraat 45, 7531 AA Enschede', 'brasseriekachel.nl'),
@@ -119,13 +133,13 @@ INSERT INTO `restaurants` (`id`, `name`, `location`, `contact`) VALUES
 (78, 'Nyonya Meneer Indonesisch Eethuis', 'Walstraat 9, 7511 GE Enschede', 'nyonyameneer.nl'),
 (79, 'Snackkar aan huis \"Tukker food\" betrouwbaar en betaalbaar', 'Waalstraat 50, 7523 RK Enschede', 'tukker-food.nl'),
 (80, 'Restaurant de Oude Apotheek', 'Teylersstraat 4, 7581 AH Losser', 'oude-apotheek.nl'),
-(81, 'Humphrey\'s Restaurant Enschede', 'Oude Markt 3A, 7511 GA Enschede', 'humphreys.nl'),
+(81, 'Humphrey\s Restaurant Enschede', 'Oude Markt 3A, 7511 GA Enschede', 'humphreys.nl'),
 (82, 'Los Ponchos', 'Korte Haaksbergerstraat 2, 7511 JS Enschede', 'losponchos.nl'),
 (83, 'Yuzu Enschede', 'Van Loenshof 35, 7511 HE Enschede', 'yuzuenschede.nl'),
 (84, 'Masada', 'Wethouder Gerbertstraat 61, 7543 AW Enschede', 'masada.nl'),
 (85, 'Steakhouse El Gaucho', 'Zuiderhagen 16, 7511 GL Enschede', 'el-gaucho-enschede.nl'),
 (86, 'Eetcafé Bij Flip', 'Langestraat 58-60, 7511 HC Enschede', 'eetcafebijflip.nl'),
-(87, 'Vaperansa\'s Soul Food', 'Hoge Bothofstraat 39A, 7514 ZA Enschede', 'vaperansa.nl'),
+(87, 'Vaperansa\s Soul Food', 'Hoge Bothofstraat 39A, 7514 ZA Enschede', 'vaperansa.nl'),
 (88, 'Moeke Enschede', 'Oude Markt 8, 7511 GA Enschede', 'moekeenschede.nl'),
 (89, 'Restaurant & Grand Café The Gallery Enschede', 'Hengelosestraat 500, 7521 AN Enschede', 'gallerycatering.nl'),
 (90, 'Cafetaria Sylvia', 'Rijnstraat 74, 7523 GH Enschede', 'sylvia-enschede.nl'),
@@ -134,8 +148,8 @@ INSERT INTO `restaurants` (`id`, `name`, `location`, `contact`) VALUES
 (93, 'Light of India', 'Emmastraat 189, 7513 BC Enschede', 'ecocentrumemma.nl'),
 (94, 'Happy Italy', 'Willem Wilminkplein 31, 7511 PP Enschede', 'happyitaly.nl'),
 (95, 'De Pauw', 'Deurningerstraat 217, 7522 CB Enschede', ''),
-(96, 'Bistro C\'est Si Bon', 'Noorderhagen 54A, 7511 EM Enschede', 'cestsibon.nl'),
-(97, 'Mario\'s en Mario\'s Sweets', 'Hengelosestraat 184B, 7521 AK Enschede', 'marios-burgers.nl'),
+(96, 'Bistro C\est Si Bon', 'Noorderhagen 54A, 7511 EM Enschede', 'cestsibon.nl'),
+(97, 'Mario\s en Mario\s Sweets', 'Hengelosestraat 184B, 7521 AK Enschede', 'marios-burgers.nl'),
 (98, 'Eetcafe ’t Raedthuys', 'Sint Maartenstraat 57, 7581 AK Losser', 'eetcaferaedthuys.nl'),
 (99, 'Restaurant De Stadsgracht', 'Stadsgravenstraat 57, 7511 ER Enschede', 'destadsgracht.nl'),
 (100, 'Restaurant ZIN', 'Noord Esmarkerrondweg 421-2, 7533 BL Enschede', 'restaurantzin.nl'),
@@ -144,7 +158,7 @@ INSERT INTO `restaurants` (`id`, `name`, `location`, `contact`) VALUES
 (103, 'Olympic', 'Korte Haaksbergerstraat 4, 7511 JS Enschede', 'grieksrestaurantenschede.nl'),
 (104, 'Bagels & Beans', 'Korte Hengelosestraat 33, 7511 JA Enschede', 'bagelsbeans.nl'),
 (105, 'Pulcinella B.V.', 'Deurningerstraat 91, 7514 BE Enschede', 'pizzeria-pulcinella.nl'),
-(106, 'Rosy\'s', 'Van Loenshof 86, 7511 NK Enschede', 'rosysfood.nl'),
+(106, 'Rosy\s', 'Van Loenshof 86, 7511 NK Enschede', 'rosysfood.nl'),
 (107, 'Twentse Bierbrouwerij', 'Haaksbergerstraat 51, 7554 PA Hengelo', 'twentsebierbrouwerijproeflokaal.nl'),
 (108, 'De Lunchkamer', 'Roomweg 75, 7523 BL Enschede', 'lunchkamer.com'),
 (109, 'Kleinsman Eten en Drinken', 'Weerseloseweg 356, 7522 PT Enschede', ''),
@@ -158,7 +172,7 @@ INSERT INTO `restaurants` (`id`, `name`, `location`, `contact`) VALUES
 (117, 'Restaurant De Broeierd', 'Hengelosestraat 725, 7521 PA Enschede', 'bistrodebroeierd.nl'),
 (118, 'JAMe Food Enschede', 'Willem Wilminkplein 27a, 7511 PP Enschede', 'jamefood.com'),
 (119, 'Restaurant Fleur De Sel', 'Beckumerstraat 20, 7548 BG Enschede', 'restaurantfleurdesel.nl'),
-(120, 'Shivani\'s Surinaams Eethuis', 'Lipperkerkstraat 33, 7511 CT Enschede', 'shivanis.nl'),
+(120, 'Shivani\s Surinaams Eethuis', 'Lipperkerkstraat 33, 7511 CT Enschede', 'shivanis.nl'),
 (121, 'The Bombay Spice', 'Wemenstraat 59, 7551 EW Hengelo', 'bombayspice.nl'),
 (122, 'La Place', 'Raadhuisstraat 12, 7511 HK Enschede', 'laplace.com');
 
@@ -169,42 +183,18 @@ INSERT INTO `restaurants` (`id`, `name`, `location`, `contact`) VALUES
 --
 
 CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
-  `token` varchar(255) DEFAULT NULL
+  `userid` int(25) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`id`, `token`) VALUES
-(4, '1234567890'),
-(5, '1234567890'),
-(6, '1234567890'),
-(7, '1234567890'),
-(8, '1234567890'),
-(9, '1234567890'),
-(10, '1234567890'),
-(11, '0987654321'),
-(12, '1234567890abcdef'),
-(13, '1234567890abcdef'),
-(14, '1234567890abcdef'),
-(15, '1234567890abcdef'),
-(16, '1234567890abcdef'),
-(17, 'someverylongrandomtoken'),
-(18, 'randomrandomrandom'),
-(19, 'someverylongrandomtoken'),
-(20, 'randomrandomrandom'),
-(21, 'someverylongrandomtoken'),
-(22, 'randomrandomrandom'),
-(23, 'someverylongrandomtoken'),
-(24, 'randomrandomrandom'),
-(25, 'someverylongrandomtoken'),
-(26, 'randomrandomrandom');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `checkin`
+--
+ALTER TABLE `checkin`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `restaurants`
@@ -216,23 +206,23 @@ ALTER TABLE `restaurants`
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`userid`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
+-- AUTO_INCREMENT for table `checkin`
+--
+ALTER TABLE `checkin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `restaurants`
 --
 ALTER TABLE `restaurants`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=123;
-
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- sql.js
+++ sql.js
@@ -1,5 +1,4 @@
 const mysql = require('mysql');
-// const config = require('./config');
 const express = require("express");
 const app = express();
 
@@ -10,30 +9,8 @@ const pool = mysql.createPool({
     database: 'appDb'
 });
 
-
-
-function createDB(connection){
-    connection.query("CREATE DATABASE appDB", function(error, result){
-        if (error) {
-            console.log('Database already exists!');
-        }else{
-            console.log("Database was created!");
-        }
-    });
-}
-
-function createTable(connection,sql){
-    connection.query(sql, function (error, result) {
-        if (error) {
-            console.log('Table already exists!');
-        } else{
-            console.log("Table created!");
-        }
-    });
-}
-
 function addUser(data){
-    let inserttoDB = 'INSERT INTO users (token) VALUES ?'; // user, id,token
+    let inserttoDB = 'INSERT INTO users (userid) VALUES ?'; // user, id,token
     let query = mysql.format(inserttoDB,[data]);
     pool.query(query, (error, result) =>{
         if (error) {
@@ -43,6 +20,7 @@ function addUser(data){
         console.log("Added a user with a token" + data);
     });
 }
+// function userCheckIn()
 
 function getResults(table){
     let get = 'SELECT * FROM ??';
@@ -55,26 +33,20 @@ function getResults(table){
 app.get("/",(req,res) => {
     pool.getConnection((err,connection) => {
         if(err) throw err;
-        let data = [["someverylongrandomtoken"],["randomrandomrandom"]];
+        let data = [["123123123"],["333444555"]];
         addUser(data);
         setTimeout(() => {
             getResults("users");
         },500);
 
-    });
-    // createDB(connection);
-    // var sql = "CREATE TABLE users (id INT AUTO_INCREMENT PRIMARY KEY, token varchar(255))";
-    // createTable(connection, sql);
 
-    // var sqlInsert = "INSERT INTO restaurants (name,location,contact) VALUES ?";
-    // addToDB(connection, sqlInsert, restaurants);    
-    
+
+    });
+
+    // getResults("restaurants")
+    // getResults("checkin")
 });
 
 app.listen(3000, () => {
     console.log('Server is running at port 3000');
 });
-
-
-// restaurant table
-// users and restaurant table
