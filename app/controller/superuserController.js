29518d19dd71c06b2b5e045186b197710b14752f
s1965891 <r.b.smink@student.utwente.nl>
2020-10-01 22:45:04 +0000

    create password regex, phone number regex and max character counts.

--- app/controller/superuserController.js
+++ app/controller/superuserController.js
@@ -8,17 +8,40 @@ const SECRET_KEY = require('../../secret');
 exports.create_superuser = function(req, res){
   var newSuperuser = new Superuser(req.body);
 
+  var passwordcheck = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+  var phonenumbercheck = /^[0-9]*$/
+
   //handles errors
-  // TODO: REGEX checks (e.g. password long enough, postalcode, email)
   if(!(newSuperuser.type == "sanitary_service" || newSuperuser.type == "restaurant_owner")){
     res.status(400).send({ error:true, message: 'Please provide type of user.'});
   } else if (!newSuperuser.password || !newSuperuser.username){
     res.status(400).send({ error:true, message: 'Please provide login information of user.'});
+  } else if (!passwordcheck.test(newSuperuser.password)){
+    res.status(400).send({ error:true, message: 'Please provide a 8 character long password with at least one Uppercase letter, special character and number.'});
+  } else if (!phonenumbercheck.test(newSuperuser.phonenumber)){
+    res.status(400).send({ error:true, message: 'Please provide a phone number without letters or special characters.'});
   } else if (!newSuperuser.email || !newSuperuser.phonenumber){
     res.status(400).send({ error:true, message: 'Please provide properly formatted contact information.'});
   }else if (!newSuperuser.city || !newSuperuser.streetname || !newSuperuser.housenumber || !newSuperuser.postalcode){
     res.status(400).send({ error:true, message: 'Please provide properly formatted location details.'});
+  } else if (newSuperuser.postalcode.length >= 100){
+    res.status(400).send({ error:true, message: 'Please provide a postal code under 100 characters.'});
+  } else if (newSuperuser.housenumber.length >= 100){
+    res.status(400).send({ error:true, message: 'Please provide a house number under 100 characters.'});
+  } else if (newSuperuser.streetname.length >= 100){
+    res.status(400).send({ error:true, message: 'Please provide a street name under 100 characters.'});
+  } else if (newSuperuser.city.length >= 100){
+    res.status(400).send({ error:true, message: 'Please provide a city under 100 characters.'});
+  } else if (newSuperuser.phonenumber.length >= 100){
+    res.status(400).send({ error:true, message: 'Please provide a phone number under 100 characters.'});
+  } else if (newSuperuser.email.length >= 100){
+    res.status(400).send({ error:true, message: 'Please provide an e-mail under 100 characters.'});
+  } else if (newSuperuser.password.length >= 100){
+    res.status(400).send({ error:true, message: 'Please provide a password under 100 characters.'});
+  } else if (newSuperuser.username.length >= 100){
+    res.status(400).send({ error:true, message: 'Please provide a username under 100 characters.'});
   }else{
+
   Superuser.createSuperuser(newSuperuser, function(err, superuser) {
     if (err){
       res.send({ error:true, message: err});
