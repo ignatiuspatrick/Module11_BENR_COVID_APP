bba6113ef3c8491e5a3ee0c757eaaff03d5074bc
r.r.govers@student.utwente.nl <r.r.govers@student.utwente.nl>
2020-10-06 16:29:55 +0200

    Fixed error and added comments. GGD code generation works.

--- app/model/userModel.js
+++ app/model/userModel.js
@@ -61,28 +61,32 @@ User.deleteUser = function (userId, result) {
 User.generateCode = function(userId, result){
 
   //First check if a code is available (that is still valid, e.g. less than a day old).
-  sql.query("SELECT code FROM ggd_codes WHERE id = ? AND created_at >= now() + INTERVAL 1 DAY;", userId, function(err,res){
-    // TODO: Fix this. generates new one each time.
-    console.log(res);
-    if(err){
+  sql.query("SELECT code FROM ggd_codes WHERE userid = ? AND `created_at` > timestampadd(hour, -24, now());", userId, function(err,queryresult){
+    console.log(queryresult);
+    if(err){ //error handling
       return result(err, null);
-    }else if(res.length > 0){
-      return result(null, res);
-    }
-  });
+    }else if(queryresult.length == 1){ //we have a valid result!
+      return result(null, queryresult[0].code);
+    } else if (queryresult.length == 0){ //We don't have a valid result yet so we generate one.
 
-  //Did not return yet => now we have to generate a code ourselves.
-  let temp_code = cryptoRandomString({length: 8, type: 'distinguishable'});
-  console.log(temp_code);
-  sql.query("INSERT INTO ggd_codes SET userid = ?, code = ?", [userId, temp_code], function(err,res){
-    if (err) {
-        if(err.code == 'ER_DUP_ENTRY' || err.errno == 1062){ //duplicate entry, very rare error but might wanna clarify if it happens
-          return result("Please try again, there was a duplicate code in the database.",null);
-      }else{
-        return result(err, null);
-      }
-    } else {
-      return result(null, temp_code);
+      //Did not return yet => now we have to generate a code ourselves.
+      let temp_code = cryptoRandomString({length: 8, type: 'distinguishable'});
+      console.log(temp_code);
+      sql.query("INSERT INTO ggd_codes SET userid = ?, code = ?", [userId, temp_code], function(err,queryresult){
+        if (err) {
+           //duplicate entry, very rare error but might wanna clarify if it happens, can just try again in this case.
+          if(err.code == 'ER_DUP_ENTRY' || err.errno == 1062){
+              return result("Please try again, there was a duplicate code in the database. (mysqlcode 1062)",null);
+          }else{ //different errors
+            return result(err, null);
+          }
+        } else { //eyy no errors, succesfully made a new code. Return it to user.
+          return result(null, temp_code);
+        }
+      });
+
+    } else { //Ya idk too many queryresult entries. So I guess we gotta give an error.
+      return result("Something went wrong, too many codes available.", null);
     }
   });
 
