598a4028be241367549ddf11889df3b1af6d23f8
Ignatius Patrick <ignatiuspatrick@student.utwente.nl>
2020-10-15 14:51:44 +0200

    frontend resto info

--- views/src/layouts/RestoOwnerDash.js
+++ views/src/layouts/RestoOwnerDash.js
@@ -43,8 +43,8 @@ export default function RestoOwnerDash({ ...rest }) {
   // ref to help us initialize PerfectScrollbar on windows devices
   const mainPanel = React.createRef();
   // states and functions
-  const [image, setImage] = React.useState(bgImage);
-  const [color, setColor] = React.useState("peach");
+  const [image] = React.useState(bgImage);
+  const [color] = React.useState("peach");
   const [mobileOpen, setMobileOpen] = React.useState(false);
   const handleDrawerToggle = () => {
     setMobileOpen(!mobileOpen);
--- views/src/views/UserProfile/ROInformation.js
+++ views/src/views/UserProfile/ROInformation.js
@@ -2,6 +2,7 @@ import React from "react";
 // @material-ui/core components
 import { makeStyles } from "@material-ui/core/styles";
 import InputLabel from "@material-ui/core/InputLabel";
+import TextField from '@material-ui/core/TextField';
 // core components
 import GridItem from "components/Grid/GridItem.js";
 import GridContainer from "components/Grid/GridContainer.js";
@@ -9,11 +10,8 @@ import CustomInput from "components/CustomInput/CustomInput.js";
 import Button from "components/CustomButtons/Button.js";
 import Card from "components/Card/Card.js";
 import CardHeader from "components/Card/CardHeader.js";
-import CardAvatar from "components/Card/CardAvatar.js";
 import CardBody from "components/Card/CardBody.js";
 import CardFooter from "components/Card/CardFooter.js";
-
-import avatar from "assets/img/faces/marc.jpg";
 
 const styles = {
   cardCategoryWhite: {
@@ -31,6 +29,9 @@ const styles = {
     fontFamily: "'Roboto', 'Helvetica', 'Arial', sans-serif",
     marginBottom: "3px",
     textDecoration: "none"
+  },
+  textFieldInfo: {
+    marginLeft: "15px"
   }
 };
 
@@ -38,6 +39,18 @@ const useStyles = makeStyles(styles);
 
 export default function ROInformation() {
   const classes = useStyles();
+
+  const [businessname, setBusinessName] = React.useState('defaultvalue'); // get thru api
+  const [city, setCity] = React.useState('defaultvalue'); // get thru api
+  const [streetname, setStreetName] = React.useState('defaultvalue'); //get thru api
+  const [number, setNumber] = React.useState('defaultvalue'); //get thru api
+  const [postalcode, setPostalCode] = React.useState('defaultvalue'); //get thru api
+
+  // for the backend
+  function updateInfo() {
+
+  }
+
   return (
     <div>
       <GridContainer>
@@ -49,105 +62,52 @@ export default function ROInformation() {
             </CardHeader>
             <CardBody>
               <GridContainer>
-                <GridItem xs={12} sm={12} md={5}>
-                  <CustomInput
-                    labelText="Company (disabled)"
-                    id="company-disabled"
-                    formControlProps={{
-                      fullWidth: true
-                    }}
-                    inputProps={{
-                      disabled: true
-                    }}
-                  />
-                </GridItem>
-                <GridItem xs={12} sm={12} md={3}>
-                  <CustomInput
-                    labelText="Username"
-                    id="username"
-                    formControlProps={{
-                      fullWidth: true
-                    }}
-                  />
-                </GridItem>
                 <GridItem xs={12} sm={12} md={4}>
-                  <CustomInput
-                    labelText="Email address"
-                    id="email-address"
-                    formControlProps={{
-                      fullWidth: true
-                    }}
+                  <TextField 
+                  id="businessname" 
+                  label="Restaurant Name"
+                  defaultValue={businessname} 
+                  fullWidth
+                  onChange={(e) => setBusinessName(e.target.value)}
                   />
-                </GridItem>
-              </GridContainer>
-              <GridContainer>
-                <GridItem xs={12} sm={12} md={6}>
-                  <CustomInput
-                    labelText="First Name"
-                    id="first-name"
-                    formControlProps={{
-                      fullWidth: true
-                    }}
+                  <TextField 
+                  id="city" 
+                  label="City"
+                  defaultValue={city} 
+                  fullWidth
+                  onChange={(e) => setCity(e.target.value)}
                   />
-                </GridItem>
-                <GridItem xs={12} sm={12} md={6}>
-                  <CustomInput
-                    labelText="Last Name"
-                    id="last-name"
-                    formControlProps={{
-                      fullWidth: true
-                    }}
+                  <TextField 
+                  id="streetname" 
+                  label="Street Name"
+                  defaultValue={streetname} 
+                  fullWidth
+                  onChange={(e) => setStreetName(e.target.value)}
                   />
-                </GridItem>
-              </GridContainer>
-              <GridContainer>
-                <GridItem xs={12} sm={12} md={4}>
-                  <CustomInput
-                    labelText="City"
-                    id="city"
-                    formControlProps={{
-                      fullWidth: true
-                    }}
+                  <TextField
+                  id="number"
+                  label="Number"
+                  type="number"
+                  defaultValue={number}
+                  fullWidth
+                  onChange={(e) => setNumber(e.target.value)}
                   />
-                </GridItem>
-                <GridItem xs={12} sm={12} md={4}>
-                  <CustomInput
-                    labelText="Country"
-                    id="country"
-                    formControlProps={{
-                      fullWidth: true
-                    }}
-                  />
-                </GridItem>
-                <GridItem xs={12} sm={12} md={4}>
-                  <CustomInput
-                    labelText="Postal Code"
-                    id="postal-code"
-                    formControlProps={{
-                      fullWidth: true
-                    }}
-                  />
-                </GridItem>
-              </GridContainer>
-              <GridContainer>
-                <GridItem xs={12} sm={12} md={12}>
-                  <InputLabel style={{ color: "#AAAAAA" }}>About me</InputLabel>
-                  <CustomInput
-                    labelText="Lamborghini Mercy, Your chick she so thirsty, I'm in that two seat Lambo."
-                    id="about-me"
-                    formControlProps={{
-                      fullWidth: true
-                    }}
-                    inputProps={{
-                      multiline: true,
-                      rows: 5
-                    }}
+                  <TextField 
+                  id="postalcode" 
+                  label="Postal Code"
+                  defaultValue={postalcode} 
+                  fullWidth
+                  onChange={(e) => setPostalCode(e.target.value)}
                   />
                 </GridItem>
               </GridContainer>
             </CardBody>
             <CardFooter>
-              <Button color="info">Update Profile</Button>
+              <Button 
+              color="info"
+              onClick={updateInfo}>
+                Update Details
+              </Button>
             </CardFooter>
           </Card>
         </GridItem>
