fdf15cbaf71f1647e5ebef632afc72e869046a19
Konstantin <konstantinas.averkin@gmail.com>
2020-10-20 13:30:50 +0200

    fix for Patrick

--- views/src/views/Dashboard/RODashboard.js
+++ views/src/views/Dashboard/RODashboard.js
@@ -19,12 +19,9 @@ import CardIcon from "components/Card/CardIcon.js";
 import CardBody from "components/Card/CardBody.js";
 import CardFooter from "components/Card/CardFooter.js";
 
-import { jsPDF } from "jspdf";
-
 import back from "../../hosts.js";
 
 import styles from "assets/jss/material-dashboard-react/views/dashboardStyle.js";
-
 
 const useStyles = makeStyles(styles);
 
@@ -32,15 +29,7 @@ export default function Dashboard() {
   const classes = useStyles();
   // const [code, setCode] = React.useState('');
   const [errorflag, setErrorflag] = React.useState(0);
-  // const [name,setName] = React.useState('');
-  // const [ownerid,setId] = React.useState(0);
   const username = localStorage.getItem('name');
-  // const [restid, setRestId] = React.useState(0);
-  // const [restname, setRestName] = React.useState('');
-  // const [streetname, setStreetName] = React.useState('');
-  // const [number, setNumber] = React.useState('');
-  // const [postalcode, setPostalCode] = React.useState('');
-  // const [city, setCity] = React.useState('');
   const [qrvalue, setQRValue] = React.useState('123456'); // get from the backend later
   const [qrformat, setQRFormat] = React.useState('');
   const [novisitors, setNovisitors] = React.useState(0);
@@ -63,74 +52,66 @@ export default function Dashboard() {
   // for stats
   var counter = new Date(today.getFullYear, today.getMonth, 0).getDate();
 
+  const [ownerid,setId] = React.useState(0);
+  const [restid, setRestId] = React.useState(0);
   //might be useful for later
-  // React.useEffect(()=>{
-  //   async function getId(){
-  //     const request = require('request');
-  //     let options = {
-  //       uri: back + '/superusers/getid',
-  //       withCredentials: true
-  //     };
-  //     request.get(options,(err,res,body)=>{
-  //       if (err) {
-  //         return console.log(err);
-  //       }
-  //       if(res.statusCode === 200){
-  //         var obj=JSON.parse(body);
-  //         setId(obj.id);
-  //         // var ownerid = obj.id;
-  //         // localStorage.setItem('ownerid',ownerid);
-  //         const request2 = require('request');
-  //         let options2 = {
-  //         uri: back + '/restaurants/getrest',
-  //         withCredentials: true,
-  //         form: {
-  //           ownerid: obj.id
-  //         }
-  //       }
-  //       request2.post(options2,(err,res,body)=>{
-  //         if (err) {
-  //           return console.log(err);
-  //         }
-  //         if(res.statusCode === 200){
-  //           var obj = JSON.parse(body);
-  //           setRestId(obj[0].id);
-  //           // display in dashboard this data 
-  //           setRestName(obj[0].name);
-  //           setStreetName(obj[0].streetname);
-  //           setNumber(obj[0].number);
-  //           setPostalCode(obj[0].postalcode);
-  //           setCity(obj[0].city);
-  //         }
-  //       });
+  React.useEffect(()=>{
+    async function getId(){
+      const request = require('request');
+      let options = {
+        uri: back + '/superusers/getid',
+        withCredentials: true
+      };
+      request.get(options,(err,res,body)=>{
+        if (err) {
+          return console.log(err);
+        }
+        if(res.statusCode === 200){
+          var obj=JSON.parse(body);
+          setId(obj.id);
+          
+          const request2 = require('request');
+          let options2 = {
+          uri: back + '/restaurants/getrest',
+          withCredentials: true,
+          form: {
+            ownerid: obj.id
+          }
+        }
+        request2.post(options2,(err,res,body)=>{
+          if (err) {
+            return console.log(err);
+          }
+          if(res.statusCode === 200){
+            var obj = JSON.parse(body);
+            setRestId(obj[0].id);
+          }
+        });
 
-  //       }
-  //     });
-  //   }
-  //   getId();
-  // },[]);
-
-  // const onFormSubmit = e => {
-  //   e.preventDefault();
-  //   registerPersonnel();
-  //   e.target.reset();
-  // }
+        }
+      });
+    }
+    getId();
+  },[]);
+console.log("restid: " + restid);
 
   function getNOVisitors(type) {
     const request3 = require('request');
     let options3 = {
       uri: back + '/superusers/visited/',
+      withCredentials:true,
       form: {
-        restid: 122, // restoid for laplace, might want to fix later
+        ownerid: ownerid,
+        restid: restid, // might want to fix later
         days: type
       }
     }
-    request3.get(options3, (err,res,body) => {
+    request3.post(options3, (err,res,body) => {
       if (err) {
         return console.log(err);
       } else if (res.statusCode === 200) {
         var obj = JSON.parse(body);
-        setNovisitors(obj[0].visited)
+        setNovisitors(obj.result);
       }
     })
   }
@@ -152,25 +133,18 @@ export default function Dashboard() {
 
   function downloadQRCode() {
     console.log('downloaded format ' + qrformat);
-    var url;
-    const canvas = document.getElementById("qrimg");
     if (qrformat === "PNG"){
-      url = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
-    } else if (qrformat === "JPEG") {
-      url = canvas.toDataURL('image/jpeg', 1.0);
-    } else if (qrformat === "PDF") {
-      url = canvas.toDataURL("image/jpeg", 1.0);
-      var pdf = new jsPDF();
-      pdf.addImage(url, 'JPEG', 0, 0);
-      pdf.save("123456.pdf");
-      return null; // terminate
+      const canvas = document.getElementById("qrimg");
+      const pngUrl = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
+      let downloadLink = document.createElement("a");
+      downloadLink.href = pngUrl;
+      downloadLink.download = "123456.png";
+      document.body.appendChild(downloadLink);
+      downloadLink.click();
+      document.body.removeChild(downloadLink);
+    } else {
+      console.log("additional format coming soon!")
     }
-    let downloadLink = document.createElement("a");
-    downloadLink.href = url;
-    downloadLink.download = "123456." + qrformat.toLowerCase();
-    document.body.appendChild(downloadLink);
-    downloadLink.click();
-    document.body.removeChild(downloadLink);
   }
   
   return (
@@ -248,9 +222,9 @@ export default function Dashboard() {
               <div className={classes.stats}>
                 <DateRange />
                 <ButtonGroup color="inherit" aria-label="outlined primary button group" size="small" style={{marginLeft: 10}}>
-                  <Button onClick={getNOVisitors(0)}>Today</Button>
-                  <Button onClick={getNOVisitors(6)}>Week</Button>
-                  <Button onClick={getNOVisitors(counter - 1)}>Month</Button> 
+                  <Button onClick={()=> getNOVisitors(0)}>Today</Button>
+                  <Button onClick={()=>getNOVisitors(6)}>Week</Button>
+                  <Button onClick={()=>getNOVisitors(counter - 1)}>Month</Button> 
                 </ButtonGroup>
               </div>
             </CardFooter>
