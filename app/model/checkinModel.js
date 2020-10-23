81497eddd064bc7853378ebdf95b2a94c914bf29
r.r.govers@student.utwente.nl <r.r.govers@student.utwente.nl>
2020-10-23 11:25:58 +0200

    Can't checkin when infected now.

--- app/model/checkinModel.js
+++ app/model/checkinModel.js
@@ -12,38 +12,46 @@ var Checkin = function(checkin){
 //SQL Queries for checkin in
 //Restaurant is gathered from the unique
 Checkin.createCheckin = function (userId, code, result) {
-  //Select restaurant id from restaurrant_codes using code
+  //Select restaurant id from restaurant_codes using code
   sql.query("SELECT restid FROM restaurant_codes WHERE code = ?", [code], function (err, id) {
     if(err) {
       console.log(err);
       return result(err, null);
     } else if (id.length < 1) {
       console.log(err);
-      return result(1, "No associated restaurant id")
-    }
-    else{
-      //Select restaurant id from restaurrant_codes using code
+      return result(1, "No associated restaurant id");
+    }else{
       sql.query("SELECT ToS FROM restaurants WHERE id = ?", [id[0].restid], function (err, tos) {
         if(err) {
           console.log(err);
           return result(err, null);
-        }
-        else{
+        } else if(!tos) {
+          return result("No ToS found.", null);
+        } else {
+
+          //check if someone is infected before checking someone in
+          sql.query("SELECT infected FROM users WHERE id = ?", userId, function(err, infected){
+            console.log(infected);
+            if(infected[0].infected){ //also for at risk?
+              return result(null, -1); //infected, should not get access
+            }
+          });
           //Create user checkin
-        var cot = new Date()
-        var addedtime = tos[0].ToS.split(":")
-        cot.setSeconds(cot.getSeconds() + parseInt(addedtime[2]));
-        cot.setMinutes(cot.getMinutes() + parseInt(addedtime[1]));
-        cot.setHours(cot.getHours() + parseInt(addedtime[0]));
-        sql.query("INSERT INTO checkin SET userid = ?, restid = ?, checkin_time = ?, checkout_time = ?, at_risk = 0", [userId, id[0].restid, new Date(), cot], function (err, res) {
-          if(err) {
-              console.log(err);
-              return result(err, null);
-          }
-          else{
-              console.log(res.insertId); //we return the checkinId
-              result(null, res.insertId);
-          }
+          var cot = new Date()
+          var addedtime = tos[0].ToS.split(":")
+          cot.setSeconds(cot.getSeconds() + parseInt(addedtime[2]));
+          cot.setMinutes(cot.getMinutes() + parseInt(addedtime[1]));
+          cot.setHours(cot.getHours() + parseInt(addedtime[0]));
+          //// TODO: Is at_risk = 0 a good idea here?? Probably not.
+          sql.query("INSERT INTO checkin SET userid = ?, restid = ?, checkin_time = ?, checkout_time = ?, at_risk = 0", [userId, id[0].restid, new Date(), cot], function (err, res) {
+            if(err) {
+                console.log(err);
+                return result(err, null);
+            }
+            else{
+                console.log(res.insertId); //we return the checkinId
+                result(null, res.insertId);
+            }
         });
       }
       });
