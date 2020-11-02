0385bb1887b994a4f9e50c038f1ab2e2541f4668
Konstantin <konstantinas.averkin@gmail.com>
2020-11-02 10:48:06 +0100

    port change for front-end

--- views/gulpfile.js
+++ views/gulpfile.js
@@ -8,18 +8,7 @@ gulp.task("licenses", async function() {
     .pipe(
       gap.prependText(`/*!
 
-=========================================================
-* Material Dashboard React - v1.9.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/material-dashboard-react
-* Copyright 2020 Creative Tim (http://www.creative-tim.com)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+MINDHASH
 
 */`)
     )
@@ -31,18 +20,7 @@ gulp.task("licenses", async function() {
     .pipe(
       gap.prependText(`<!--
 
-=========================================================
-* Material Dashboard React - v1.9.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/material-dashboard-react
-* Copyright 2020 Creative Tim (http://www.creative-tim.com)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+MINDHASH
 
 -->`)
     )
@@ -54,18 +32,7 @@ gulp.task("licenses", async function() {
     .pipe(
       gap.prependText(`/*!
 
-=========================================================
-* Material Dashboard React - v1.9.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/material-dashboard-react
-* Copyright 2020 Creative Tim (http://www.creative-tim.com)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+MINDHASH
 
 */`)
     )
